54e2879214d22262bcc88acfa83eba92a251fb0b
shoaibakhterqadri <shoaiakhter181422@gmail.com>
2023-03-19 19:43:37 +0500

    Weather Application is completed.

--- src/app.js
+++ src/app.js
@@ -1,28 +1,41 @@
 const express=require('express');
 const path=require('path');
+const hbs=require('hbs')
 const app=express();
 const port=process.env.PORT || 3000;
 
 // Public static path
 const static_path = path.join(__dirname,"../public")
+const template_path = path.join(__dirname,"../templates/views")
+const partials_path = path.join(__dirname,"../templates/partials")
+
+
+
+app.set('view engine','hbs')
+app.set('views',template_path)
+hbs.registerPartials(partials_path)
 
 app.use(express.static(static_path))
+
 // Routing
 
 app.get('/',(req,res)=>{
-      res.send("Shoaib Akhter")
+      res.render('index')
 });
 
 app.get('/about',(req,res)=>{
-      res.send("About")
+      res.render('about')
+
 });
 
 app.get('/weather',(req,res)=>{
-      res.send("Weature")
+      res.render("weather")
 });
 
 app.get('*',(req,res)=>{
-      res.send("404 error")
+      res.render("404error",{
+            errorMsg:"Opps! Page Not Found"
+      })
 });
 
 app.listen(port,()=>{
